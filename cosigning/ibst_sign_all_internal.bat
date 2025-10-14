a9d74888f88396aaba0a2a19cb17bf1f291fba9a
Cuevas Ramirez, Jaime <jaime.cuevas.ramirez@intel.com>
2025-10-14 13:19:41 -0600

    adding tool signing

--- cosigning/ibst_sign_all_internal.bat
+++ cosigning/ibst_sign_all_internal.bat
@@ -25,6 +25,14 @@ if %errorlevel% neq 0 (
     exit /b %errorlevel%
 )
 
+echo                 ################# Signing msrtest.efi tool #################
+"%PY_PATH%" "%IBST_DIR%\ibst.py" "%IBST_DIR%\config\CoSigningManifest.xml" --config_override "%OVERRIDE_DIR%\CoSigningManifest_msrtest_override.xml" -s key="%KEY_DIR%\3k_test_key_private.pem" module_bin="%INPUT_DIR%\msrtest.efi" output_name="%OUTPUT_DIR%\msrtest.efi.bin"
+if %errorlevel% neq 0 (
+    echo ERROR: Failed to sign msrtest.efi - Error code: %errorlevel%
+    pause
+    exit /b %errorlevel%
+)
+
 echo.
 echo Script completed successfully!
 pause
